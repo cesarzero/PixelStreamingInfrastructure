43ce52127214a73eae2b270bcccfb7b6afd2dd80
cesar0 <451591198@qq.com>
2024-03-13 10:36:43 +0800

    cesar0
    update

--- Frontend/implementations/typescript/src/player.ts
+++ Frontend/implementations/typescript/src/player.ts
@@ -1,32 +1,54 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 import { Config, PixelStreaming } from '@epicgames-ps/lib-pixelstreamingfrontend-ue5.4';
-import { Application, PixelStreamingApplicationStyle } from '@epicgames-ps/lib-pixelstreamingfrontend-ui-ue5.4';
+import { Application, PixelStreamingApplicationStyle, UIElementCreationMode } from '@epicgames-ps/lib-pixelstreamingfrontend-ui-ue5.4';
 const PixelStreamingApplicationStyles =
     new PixelStreamingApplicationStyle();
 PixelStreamingApplicationStyles.applyStyleSheet();
 
-// expose the pixel streaming object for hooking into. tests etc.
 declare global {
     interface Window { pixelStreaming: PixelStreaming; }
 }
 
 document.body.onload = function() {
-	// Example of how to set the logger level
-	// Logger.SetLoggerVerbosity(10);
-
-	// Create a config object
 	const config = new Config({ useUrlParams: true });
-
-	// Create a Native DOM delegate instance that implements the Delegate interface class
 	const stream = new PixelStreaming(config);
-
 	const application = new Application({
 		stream,
-		onColorModeChanged: (isLightMode) => PixelStreamingApplicationStyles.setColorMode(isLightMode)
+		onColorModeChanged: (isLightMode) => PixelStreamingApplicationStyles.setColorMode(isLightMode),
+		settingsPanelConfig:{
+			isEnabled:false,
+			visibilityButtonConfig:{
+				creationMode:UIElementCreationMode.Disable
+			}
+		},
+		statsPanelConfig:{
+			isEnabled:false,
+			visibilityButtonConfig:{
+				creationMode:UIElementCreationMode.Disable
+			}
+		},
+		fullScreenControlsConfig:{
+			creationMode:UIElementCreationMode.Disable
+		},
+		videoQpIndicatorConfig:{
+			disableIndicator:true
+		}
 	});
-	// document.getElementById("centrebox").appendChild(application.rootElement);
 	document.body.appendChild(application.rootElement);
-	
+	stream.addResponseEventListener("handle_responses",myHandleResponseFunction);
 	window.pixelStreaming = stream;
 }
+
+/**
+ * 接收UE的消息
+ * @param data 消息主题
+ */
+function myHandleResponseFunction(data:string) {
+	console.warn("Response received!");
+	console.log(data);
+}
+
+
+/*function SendMessage(){
+	window.pixelStreaming.emitUIInteraction("test");
+	console.log("send message");
+}*/
